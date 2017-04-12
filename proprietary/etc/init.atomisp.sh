8c29952706788001c0134be7dac4268070372710
tank0412 <romabukhtiarov@gmail.com>
2017-04-12 04:59:31 +0300

    latte: re-import blobs from Miui stable v8.2.2.0

--- proprietary/etc/init.atomisp.sh
+++ proprietary/etc/init.atomisp.sh
@@ -6,9 +6,10 @@
 # we cannot do this from init.rc directly because atomisp wants to
 # make a request_firmware call from its module init function, which
 # init won't service until the module load completes!
-insmod /system/lib/modules/videobuf-core.ko
-insmod /system/lib/modules/videobuf-vmalloc.ko
-insmod /system/lib/modules/s5k4h8drv.ko
-insmod /system/lib/modules/t4ka3drv.ko
-insmod /system/lib/modules/ov5693.ko up_delay=3
-insmod /system/lib/modules/atomisp-css2401a0_v21.ko dypool_enable=y repool_pgnr=32768
+modules=`getprop ro.modules.location`
+insmod $modules/videobuf-core.ko
+insmod $modules/videobuf-vmalloc.ko
+insmod $modules/s5k4h8drv.ko
+insmod $modules/t4ka3drv.ko
+insmod $modules/ov5693.ko up_delay=3
+insmod $modules/atomisp-css2401a0_v21.ko dypool_enable=y repool_pgnr=32768
--- proprietary/etc/init.post_boot.sh
+++ proprietary/etc/init.post_boot.sh
@@ -3,3 +3,19 @@
 # set sno
 setprop ro.sys.oem.sno `/system/bin/acd-utils r 7 | tail -n 1`
 setprop permanent.hw.custom.serialno `/system/bin/acd-utils r 17 | tail -n 1`
+echo > /sys/devices/system/cpu/cpu1/online
+echo > /sys/devices/system/cpu/cpu2/online
+echo > /sys/devices/system/cpu/cpu3/online 
+sleep 25
+mount -o rw,remount /system
+echo -n 1840000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo -n 1840000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo -n 1840000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo -n 1840000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+echo "1840000" > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+echo "90" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+echo "1680000" > /sys/devices/system/cpu/cpufreq/interactive/touchboost_freq
+echo "40000" > /sys/devices/system/cpu/cpufreq/interactive/touchboostpulse_duration
+echo "99" > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+echo 200 >/sys/class/drm/card0/gt_min_freq_mhz
+mount -o ro,remount /system
