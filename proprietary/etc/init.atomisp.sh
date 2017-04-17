119e19e5f55f3721e0d98e654149b7d9711030ff
tank0412 <romabukhtiarov@gmail.com>
2017-04-17 17:38:34 +0300

    [REVERTME] latte: add temp fix to load sensor modules

--- proprietary/etc/init.atomisp.sh
+++ proprietary/etc/init.atomisp.sh
@@ -8,6 +8,18 @@
 # init won't service until the module load completes!
 modules=`getprop ro.modules.location`
 insmod $modules/snd-soc-tfa98xx.ko
+insmod $modules/heci.ko
+insmod $modules/heci-ish.ko
+insmod $modules/hid-heci-ish.ko
+insmod $modules/sens-col-core.ko
+insmod $modules/hid-sensor-hub.ko
+insmod $modules/hid-sensor-accel-3d.ko
+insmod $modules/hid-sensor-als.ko
+insmod $modules/hid-sensor-gyro-3d.ko
+insmod $modules/hid-sensor-iio-common.ko
+insmod $modules/hid-sensor-incl-3d.ko
+insmod $modules/hid-sensor-magn-3d.ko
+insmod $modules/hid-sensor-trigger.ko
 insmod $modules/videobuf-core.ko
 insmod $modules/videobuf-vmalloc.ko
 insmod $modules/s5k4h8drv.ko
